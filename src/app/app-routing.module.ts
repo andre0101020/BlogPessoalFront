454215258a97bb2b137eed0ad0b25219a0283c01
unknown <andre.dev.oliveira@gmail.com>
2022-07-04 07:59:04 -0300

    rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path: 'cadastrar', component: CadastrarComponent},
+   {path: 'entrar', component: EntrarComponent},
+   {path: 'manu', component: MenuComponent},
+   
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
